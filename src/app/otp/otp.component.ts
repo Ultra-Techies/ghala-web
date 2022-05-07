760719425b2477922dce60da2cda1de9684271b4
malcolmmaima <malcolmmaima@gmail.com>
2022-05-07 21:33:27 +0300

    login/registration complete flow

--- src/app/otp/otp.component.ts
+++ src/app/otp/otp.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import Utils from 'app/helpers/Utils';
 
 @Component({
   selector: 'app-verification',
@@ -8,16 +10,74 @@ import { Router } from '@angular/router';
   styleUrls: ['./otp.component.css'],
 })
 export class OtpComponent implements OnInit {
-  constructor(private formBuilder: FormBuilder, private router: Router) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private http: HttpClient
+  ) {}
   otpForm!: FormGroup;
+
+  isValid = true;
+  otp: string;
+
   ngOnInit(): void {
+    console.log('State: ' + history.state.phoneNumber);
+    if (history.state.phoneNumber === undefined) {
+      this.router.navigate(['/']);
+    }
     this.otpForm = this.formBuilder.group({
       otp: ['', Validators.required],
     });
   }
+
+  onOtpChange(otp) {
+    this.otp = otp;
+  }
+
   verify() {
-    alert('Verification SuccessFull');
-    this.otpForm.reset();
-    this.router.navigate(['account-setup']);
+    this.isValid = false;
+    //check to see if history.state exists and contains otp
+    if (history.state.newUser === false) {
+      this.verifyUser(history.state.phoneNumber, this.otp);
+    } else if (
+      history.state.newUser === true &&
+      history.state.data !== undefined &&
+      history.state.data.otp === this.otp
+    ) {
+      this.isValid = true;
+      this.router.navigate(['/dashboard']);
+    } else {
+      //set error message
+      this.otpForm.controls.otp.setErrors({
+        incorrect: true,
+      });
+    }
+  }
+
+  verifyUser(phoneNumber: string, otp: string) {
+    this.http
+      .put(
+        Utils.BASE_URL + 'users',
+        { phoneNumber: phoneNumber, password: otp },
+        { headers: Utils.getHeaders() }
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+          //if data contains verified, do nothing else redirect to dashboard
+          if (data['id'] !== undefined) {
+            this.router.navigate(['/dashboard']);
+          } else {
+            //set error message
+            this.otpForm.controls.otp.setErrors({
+              incorrect: true,
+            });
+          }
+        },
+        (error) => {
+          console.log(error);
+          alert('Something went wrong!');
+        }
+      );
   }
 }
